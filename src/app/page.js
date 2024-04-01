c6f721034ca342d7ac2d6835ac3f28b581878ece
shinwc147369@gmail.com <shinwc147369@gmail.com>
2024-04-01 16:49:08 +0900

    tailwind, mui 충돌 피하는 방법

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
 "use client";
 import Button from "@mui/material/Button";
-
 export default function Home() {
   return (
     <>
-      <Button className="bg-red-500" variant="contained">
+      <Button className="tw-bg-red-500" variant="contained">
         Contained
       </Button>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  important: true,
+  prefix: "tw-",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
