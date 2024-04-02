b8e7e621b2131c4aec91d5cef73df81f659a7215
shinwc147369@gmail.com <shinwc147369@gmail.com>
2024-04-02 10:58:53 +0900

    MUI 테마 커스터마이징

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,18 @@
-import './globals.css';
+import "./globals.css";
 
 export const metadata = {
-  title: 'Main | TodoList',
-  description: 'TodoList Management Service',
+  title: "Main | TodoList",
+  description: "TodoList Management Service",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      {" "}
+      <body>
+        {children}
+        {/* <section>lorem*20</section> */}
+      </body>
       <body>{children}</body>
     </html>
   );
