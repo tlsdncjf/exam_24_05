2c111c0b5ffb580851d4d4e9162273a3e33f5ada
shinwc147369@gmail.com <shinwc147369@gmail.com>
2024-04-02 11:32:30 +0900

    MUI App Bar 컴포넌트

--- src/app/page.js
+++ src/app/page.js
@@ -4,41 +4,17 @@ import { ThemeProvider } from "@mui/material/styles";
 import { Button, Box, AppBar, Toolbar } from "@mui/material";
 import theme from "./theme";
 import { MdDeleteForever } from "react-icons/md";
-
 export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <div className="tw-flex tw-items-center tw-gap-x-3">
-          <Button variant="text" endIcon={<MdDeleteForever />}>
-            Text
-          </Button>
-          <Button
-            variant="contained"
-            startIcon={<MdDeleteForever />}
-            onClick={() => confirm("삭제할거야?")}
-          >
-            삭제
-          </Button>
-          <Button variant="outlined">Outlined</Button>
-        </div>
-
-        <div className="tw-flex tw-items-center tw-gap-x-3 tw-mt-3">
-          <Button
-            variant="text"
-            onClick={() => {
-              alert("버튼 클릭됨");
-            }}
-          >
-            Text
-          </Button>
-          <Button variant="contained" disabled>
-            Contained
-          </Button>
-          <Button variant="outlined" href="sub/">
-            sub로 이동
-          </Button>
-        </div>
+        <AppBar position="static">
+          <Toolbar className="tw-justify-center">
+            <a href="/" className="tw-font-bold">
+              NOTE!
+            </a>
+          </Toolbar>
+        </AppBar>
       </ThemeProvider>
     </>
   );
